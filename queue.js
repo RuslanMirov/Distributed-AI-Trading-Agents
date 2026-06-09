a25a25448309a96d1407ad3f2ce92e7989dcee53
RuslanMirov <ruslan777mirov@gmail.com>
2026-06-09 10:41:52 +0200

    Add queue

---
+++ queue.js
@@ -0,0 +1,11 @@
+import { Queue } from "bullmq";
+import IORedis from "ioredis";
+
+export const connection = new IORedis({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
+export const botQueue = new Queue("bot-queue", {
+  connection,
+});
